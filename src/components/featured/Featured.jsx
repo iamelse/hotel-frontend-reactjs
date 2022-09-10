35925d95bc797816dac9f5c49800a277fdaa3000
Lana Septiana <lana.septiana2@gmail.com>
2022-09-10 09:21:31 +0700

    New homepage list

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,24 @@
+import "./featured.css"
+
+function Featured() {
+  return (
+    <div className="featured">
+        <div className="featuredItem">
+            <img className="featuredImg" src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Reno</h1>
+                <h2>533 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img className="featuredImg" src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Austin</h1>
+                <h2>532 properties</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
